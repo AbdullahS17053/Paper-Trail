8c60f18bb11212719947c7fee8c1ef45bceda5c5
AbdullahS17053 <abdullah17053@gmail.com>
2023-07-07 19:03:18 +0500

    Project Added

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    private Animator anim;
+
+    [SerializeField] private float minStrikeTime;
+    [SerializeField] private float maxStrikeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        StartCoroutine("Strike");
+    }
+
+    IEnumerator Strike()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minStrikeTime, maxStrikeTime));
+            anim.SetTrigger("strike");
+        }
+    }
+}
